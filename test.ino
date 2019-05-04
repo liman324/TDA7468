26469a02677411e902a291c4914435694fdc4d54
Alexander Liman <liman324@yandex.ru>
2019-05-04 22:42:17 +0600

    Create test.ino
    Скетч компилируется, в железе не проверялось. (liman324@yandex.ru)

---
+++ test.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+#include <TDA7468.h>
+  TDA7468 tda;
+  
+void setup(){ 
+  Serial.begin(9600);Wire.begin();
+}
+
+void loop(){
+  audio();
+  delay(1000);
+}
+// https://rcl-radio.ru/wp-content/uploads/2019/05/tda7468.pdf
+void audio(){
+  tda.setInput(0); 
+  tda.setGain(0);
+  tda.setSurround(0,0,0,0);
+  tda.setVol_R(63);
+  tda.setVol_L(63);
+  tda.setTreb_Bass(0,0);
+  tda.setOutput(1); // mute
+  tda.setAlc(0,0,0,0,0,0);
+}
